6c370c2e60d000cb31f2a664f7fbd55df6b4768e
Your Name <you@example.com>
2022-03-17 05:58:20 +0000

    Add HTTPS listener; Add cert to launch script

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,10 +1,15 @@
 source aws_credentials.sh
+mkdir -p ~/.github
+echo "aws-bootstrap" > ~/.github/aws-bootstrap-repo
+echo "<username>" > ~/.github/aws-bootstrap-owner
+echo "<Github_Token>" > ~/.github/aws-bootstrap-access-token
 
 STACK_NAME=awsbootstrap
 REGION=us-east-1 
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro 
 DOMAIN=educative-cloud-course.com
+CERT=`aws acm list-certificates --region $REGION --profile awsbootstrap --output text --query "CertificateSummaryList[?DomainName=='$DOMAIN'].CertificateArn | [0]"` 
 
 GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
 GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
@@ -62,6 +67,7 @@ aws cloudformation deploy \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
     Domain=$DOMAIN \
+    Certificate=$CERT \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
